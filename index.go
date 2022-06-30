4c45f4e6c94109d9572b849e3d4db6504f06409b
koldin108902 <alex108902@naver.com>
2022-07-01 05:57:11 +0900

    add: update stocks at every minute

--- index.go
+++ index.go
@@ -3,12 +3,18 @@ package main
 import (
 	"net/http"
 	"stockServer/router"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	app := httprouter.New()
+
+	go func() {
+		router.UpdateStock()
+		time.Sleep(time.Minute * 1)
+	}()
 
 	// user
 	app.GET("/user/info/:userId", router.GetUserInfo)
---
+++ router/iterator.go
@@ -0,0 +1,81 @@
+package router
+
+import (
+	"log"
+	"math/rand"
+	"stockServer/util"
+	"strconv"
+	"time"
+)
+
+func UpdateStock() {
+	var (
+		// update query
+		query string
+
+		// only name[string] & price[int]
+		newStocks []any
+
+		// iterated count for update query
+		i int
+	)
+
+	i = 1
+
+	rows, err := db.Query(`
+		SELECT 
+			stock_name, price 
+		FROM 
+			stock_data 
+		WHERE 
+			stock_name=(
+				SELECT name FROM stocks WHERE isValid=false
+			);
+		`)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for rows.Next() {
+		var (
+			newStock util.StockData
+		)
+		query += `($,` + strconv.Itoa(i) + ` $` + strconv.Itoa(i+1) + `)`
+		varPrice := getNewStock()
+		err = rows.Scan(&newStock.Name, &newStock.Price)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		newStock.Price += varPrice
+		newStocks = append(newStocks, newStock.Name, newStock.Price)
+		i += 2
+	}
+
+	_, err = db.Exec(`
+		INSERT INTO
+			stock_data(price, stock_name) 
+		VALUES`+query, newStocks...)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func getNewStock() int {
+	s := rand.NewSource(time.Now().UnixMicro())
+	r := rand.New(s)
+
+	varPrice := r.Intn(100)
+	if varPrice > 80 {
+		varPrice = r.Intn(100)
+	}
+
+	if r.Intn(1) == 0 {
+		varPrice *= -1
+	}
+
+	return varPrice
+}
