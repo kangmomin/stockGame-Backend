d086c950fdce9f4d88be05d495c39c7c762bc6f5
koldin108902 <alex108902@naver.com>
2022-07-01 05:36:47 +0900

    update: stock data용 테이블 구조

--- util/struct_stock.go
+++ util/struct_stock.go
@@ -1,10 +1,19 @@
 package util
 
+import "time"
+
 type Stock struct {
-	StockId int      `json:"stock_id"`
-	Name    string   `json:"name"`
-	Data    []string `json:"data"`
-	Date    []string `json:"date"`
+	StockId int       `json:"stock_id"`
+	Name    string    `json:"name"`
+	ExpireT time.Time `json:"expire_t"`
+	IsValid bool      `json:"isValid"`
+}
+
+type StockData struct {
+	DataId    int       `json:"data_id"`
+	Price     int       `json:"price"`
+	UpdatedAt time.Time `json:"update_at"`
+	Name      string    `json:"name"`
 }
 
 type DealStock struct {
